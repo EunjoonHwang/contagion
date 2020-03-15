2638ab97299ec98a3d28a6bbd9b56f9818124099
eunjoon <joonybejoy@gmail.com>
2020-03-15 18:20:37 -0400

    march14data

--- data/csvToJson.js
+++ data/csvToJson.js
@@ -1,22 +1,84 @@
-// const csvToJson = require("convert-csv-to-json");
-// const fileInputName = "./corona_confirmed.csv";
-// const fileOutputName = "./data.json";
+const csv = require("jquery-csv");
+const fs = require("fs");
 
-// csvToJson.generateJsonFileFromCsv(fileInputName, fileOutputName);
+function accumulate_by_country(csv_filename) {
+  const csv_data = fs.readFileSync(csv_filename, "utf8").toString();
+  // console.log(csv_data)
+  const result = csv.toObjects(csv_data);
 
-const csvFilePath='corona_confirmed.csv'
-const csv=require('csvtojson')
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    console.log(jsonObj);
-    /**
-     * [
-     * 	{a:"1", b:"2", c:"3"},
-     * 	{a:"4", b:"5". c:"6"}
-     * ]
-     */ 
-})
- 
-// Async / await usage
-const jsonArray=await csv().fromFile(csvFilePath);
+  const new_data = {};
+  for (const idx in result) {
+    const obj = result[idx];
+    const country = obj["Country/Region"];
+    for (const key in obj) {
+      if (!["Country/Region", "Province/State", "Lat", "Long"].includes(key)) {
+        const new_key = country + "@" + key;
+        // console.log(`${key}: ${obj[key]}`)
+        // console.log(`${new_key} - ${key}: ${obj[key]}`)
+        if (new_key in new_data) {
+          new_data[new_key] += parseInt(obj[key], 10);
+        } else {
+          new_data[new_key] = parseInt(obj[key], 10);
+        }
+      }
+    }
+  }
+
+  return new_data;
+}
+
+function generate_data(new_data) {
+  const final_data = [];
+  for (const key in new_data) {
+    // console.log(`${key}: ${new_data[key]}`)
+    const value = new_data[key];
+    const keys = key.split("@");
+    const tokens = keys[1].split("/");
+    var date;
+    if (tokens[0].length === 1 && tokens[1].length === 1) {
+      date =
+        "20" +
+        tokens[2] +
+        "-0" +
+        tokens[0] +
+        "-0" +
+        tokens[1] +
+        "T00:00:00.000Z";
+    } else if (tokens[0].length === 1 && tokens[1].length > 1) {
+      date =
+        "20" +
+        tokens[2] +
+        "-0" +
+        tokens[0] +
+        "-" +
+        tokens[1] +
+        "T00:00:00.000Z";
+    } else if (tokens[0].length > 1 && tokens[1].length === 1) {
+      date =
+        "20" +
+        tokens[2] +
+        "-" +
+        tokens[0] +
+        "-0" +
+        tokens[1] +
+        "T00:00:00.000Z";
+    } else {
+      date =
+        "20" + tokens[2] + "-" + tokens[0] + "-" + tokens[1] + "T00:00:00.000Z";
+    }
+    const item = { name: keys[0], date: date, value: value, category: "test" };
+    final_data.push(item);
+  }
+  return final_data;
+}
+
+const csv_filename = "corona_confirmed.csv";
+const new_data = accumulate_by_country(csv_filename);
+const final_data = generate_data(new_data);
+var jsonData = JSON.stringify(final_data);
+// console.log(final_data);
+fs.writeFile("../src/covid_data.json", jsonData, function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
